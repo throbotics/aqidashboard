1dfd2e5934f23e0fe7985f3fa7ffbf1e9a799606
Sorapon Thammathorn <soraponth@Sorapons-MacBook-Air.local>
2025-04-10 02:01:38 +0700

    Fresh clean commit

---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import AirQualityDashboard from './pages/AirQualityDashboard';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <AirQualityDashboard />
+  </React.StrictMode>
+);
