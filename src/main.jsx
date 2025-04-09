43290d0e8a4a1fd62c35bef5d3481bbc4d8d6f17
Sorapon Thammathorn <soraponth@Sorapons-MacBook-Air.local>
2025-04-10 02:15:10 +0700

    Fix import path for AirQualityDashboard

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import AirQualityDashboard from './pages/AirQualityDashboard';
+import AirQualityDashboard from "./pages/AirQualityDashboard.js";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
