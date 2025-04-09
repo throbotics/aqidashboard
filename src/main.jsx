b6869d7a18549200bff4d55cfca27ae92ca304f0
Sorapon Thammathorn <soraponth@Sorapons-MacBook-Air.local>
2025-04-10 02:23:02 +0700

    Fix import to use .js extension

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,9 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import AirQualityDashboard from "./pages/AirQualityDashboard";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import AirQualityDashboard from "./pages/AirQualityDashboard.js"; // <-- this is the fix
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <AirQualityDashboard />
   </React.StrictMode>
