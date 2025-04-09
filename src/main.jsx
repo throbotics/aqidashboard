6a891000f372054913f66591f734a0acd7aa963b
Sorapon Thammathorn <soraponth@Sorapons-MacBook-Air.local>
2025-04-10 02:20:02 +0700

    Final fix: correct import for AirQualityDashboard.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import AirQualityDashboard from "./pages/AirQualityDashboard.js";
+import AirQualityDashboard from "./pages/AirQualityDashboard";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
