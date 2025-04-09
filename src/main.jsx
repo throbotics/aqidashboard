acec231aab51f03988089b27dc966391c27ba704
Sorapon Thammathorn <soraponth@Sorapons-MacBook-Air.local>
2025-04-10 02:26:30 +0700

    Recreate AirQualityDashboard.jsx and fix import

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import AirQualityDashboard from "./pages/AirQualityDashboard.js"; // <-- this is the fix
+import AirQualityDashboard from "./pages/AirQualityDashboard.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
